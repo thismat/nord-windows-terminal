bbd1dfe45c832f3ea1bf5bab8eaa46ce2c8a98f5
thismat <m@thismat.com>
2021-05-07 23:31:39 -0500

    Added windows fragment installation method

---
+++ install.ps1
@@ -0,0 +1,13 @@
+
+$installPath = "$Env:LocalAppData\Microsoft\Windows Terminal\Fragments\nord"
+
+if (!(Test-Path $installPath)) {
+  New-Item -Type Directory -Path $installPath
+}
+
+Copy-Item -Path nord.json -Destination $installPath
+
+Write-Output ' '
+Write-Output '❄ Nord Colorscheme Fragment Installed'
+Write-Output '  Restart windows terminal and then select or set the theme in your profile'
+Write-Output ' '
